eb66a8c6f933fc39a497eb449763acb64b090788
Deepak Dhaka <deepa.dhaka.7634@gmail.com>
2024-01-29 17:34:33 +0530

    Initial commit

---
+++ springcrud/src/main/java/com/bitsystem/springcrud/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.bitsystem.springcrud.repository;
+
+import com.bitsystem.springcrud.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    Product findByName(String name);
+}
